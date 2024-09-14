415d2fd5240e26d4ca6981df1b25140202a7d496
Hyeonho Kang <hyeonhokang10@kakao.com>
2024-09-15 01:48:59 +0900

    Localization

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
+	site: 'https://rivercor.vercel.app',
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
